811f75d7b602435db178b6225e8c4abff435188a
ManishaKapila <minisha909@gmail.com>
2019-04-19 18:40:09 +0530

    add basic29.js

---
+++ basic/basic29.js
@@ -0,0 +1,15 @@
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastNameLength=8;
+var lastName = "Anna williams";
+lastNameLength = lastName.length;
+

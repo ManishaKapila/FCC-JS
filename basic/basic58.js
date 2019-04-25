c772c6e70ca8598b76be6daa5f3701a6e1c11be2
ManishaKapila <minisha909@gmail.com>
2019-04-25 12:20:08 +0530

    add basic58.js

---
+++ basic/basic58.js
@@ -0,0 +1,10 @@
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not equal";
+  }
+  
+  // Change this value to test
+  testStrict(10);

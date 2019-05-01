3dfb8d5edf535e51570c8d9a1bcbee2ea7b9b4a6
ManishaKapila <minisha909@gmail.com>
2019-05-01 20:40:46 +0530

    add basic68.js

---
+++ basic/basic66.js
@@ -0,0 +1,15 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+   if (val <= 50 && val >= 25){
+     return "Yes";
+   }
+  return "No";
+  }
+    // Only change code above this line
+    
+  
+  // Change this value to test
+  testLogicalAnd(10);
+  
+  

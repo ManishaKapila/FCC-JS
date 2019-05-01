053ec837f27507eb6c91f05e1f1e7937ee45e5fa
ManishaKapila <minisha909@gmail.com>
2019-05-01 22:32:05 +0530

    add basic73.js

---
+++ basic/basic73.js
@@ -0,0 +1,27 @@
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch(val)
+    {
+      case 1:
+      "alpha";
+      break;
+      case 2:
+      "beta";
+      break;
+      case 3:
+      "gamma";
+      break;
+      case 4:
+      "delta";
+      break;
+    }
+    
+    
+    // Only change code above this line  
+    return answer;  
+  }
+  
+  // Change this value to test
+  caseInSwitch(1);
+  
